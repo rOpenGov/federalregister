3a46a31888b1ca981329d54013c9b26b82ae5074
leeper <thosjleeper@gmail.com>
2014-05-08 20:16:16 +0200

    complete pi_search

--- R/pi_search.r
+++ R/pi_search.r
@@ -1,14 +1,31 @@
-pi_search <- function(..., fields=NULL, per_page=NULL, page=NULL, order=NULL, version='v1') {
+pi_search <- function(..., fields=NULL, per_page=NULL, page=NULL, version='v1') {
     
+    baseurl <- paste('https://www.federalregister.gov/api/',version,'/public-inspection-documents.json?', sep='')
+    
+    query <- list(...)
     # api docs mention separate by-date API, but same as search:
     # conditions[available_on]=date
+    if('date' %in% names(query))
+        names(query)[names(query)=='date'] <- 'available_on'
+    query <- curlEscape(paste('conditions[',names(query),']=',query,'&',sep=''))
     
-    baseurl <- paste('https://www.federalregister.gov/api/',version,'/public-inspection-documents.json', sep='')
+    # handle pagination
+    if(!is.null(per_page) && as.numeric(per_page)>1000)
+        stop("'per_page' cannot be greater than 1000")
+    else if(!is.null(per_page) & !is.null(page))
+        p <- paste('per_page=',per_page,'&page=',page,sep='')
+    else if(!is.null(per_page) & is.null(page))
+        p <- paste('per_page=',per_page,sep='')
+    else if(
+        p <- paste('page=',page,sep='')
+    else 
+        p <- NULL
     
-    if(!is.null(fields))
+    if(!is.null(fields)){
         fields <- curlEscape(paste('fields[]',fields,sep='=', collapse='&'))
-        
-    args <- fields
+        args <- paste(fields,query,p,sep='&')
+    } else
+        args <- paste(query,p,sep='&')
     
     h <- basicTextGatherer()
     curlPerform(url = paste(baseurl, args, sep=''),
